50b1a815d01e0435294bb4ed18e2a4486be4b654
NGTR07 <Nicolas.Gautier@etu.univ-grenoble-alpes.fr>
2024-12-05 09:33:05 +0100

    test equipes

--- equipes.py
+++ equipes.py
@@ -5,8 +5,8 @@ Created on Wed Dec  4 13:31:53 2024
 @author: gautnico
 """
 
-import basketball_reference_scapper
-import pandas
+from basketball_reference_scraper.teams import get_roster, get_team_stats, get_team_ratings, get_team_misc
+# import pandas
 
 
 liste_equipes = [
@@ -15,32 +15,35 @@ liste_equipes = [
     "PHO", "GSW", "DEN", "LAL", "SAS", "MIN", "SAC", "POR", "UTA", "NOP"
 ]
 
-# Création d'un df pour la liste des joueurs
-for equipe in liste_equipes:
-    df = get_roster(equipe, 2025)
-    df.drop(columns=["Birth"])
-    df.to_csv(f"roster_{equipe}")
+## Création d'un df pour la liste des joueurs
+#for equipe in liste_equipes:
+#    df = get_roster(equipe, 2025)
+#    df.drop(columns=["Birth"])
+#    df.to_csv(f"roster_{equipe}")
+#    
+##    
+## Création d'un df pour les statistiques par match et par joueur de l'équipe
+#for equipe in liste_equipes:
+#    df = get_team_stats(equipe, 2025, 'PER_GAME')
+#    df = df[["G", "3P", "3P%", "2P", "2P%", "FT", "FT%", "TRB", "AST", "PTS"]]  # Colonne à garder
+#    df.to_csv(f"stats_{equipe}")
+#
+#
+## Création d'un df pour la conférence, la division et le classement
+#for equipe in liste_equipes:
+#    df = get_team_ratings(2025, equipe)
+#    df = df[["RK", "CONF", "DIV"]]
+#    df.to_csv(f"infos_base_{equipe}")
+#    
+#    
+## Création d'un df pour le stade de l'équipe
+#for equipe in liste_equipes:
+#    df = get_team_misc(equipe, 2025)
+#    df = df[["Arena"]]
+#    df.to_csv(f"stade_{equipe}")
     
-    
-# Création d'un df pour les statistiques par match et par joueur de l'équipe
-for equipe in liste_equipes:
-    df = get_team_stats(equipe, 2025, 'PER_GAME')
-    df = df[["G", "3P", "3P%", "2P", "2P%", "FT", "FT%", "TRB", "AST", "PTS"]]  # Colonne à garder
-    df.to_csv(f"stats_{equipe}")
-
-
-# Création d'un df pour la conférence, la division et le classement
-for equipe in liste_equipes:
-    df = get_team_ratings(2025, equipe)
-    df = df[["RK", "CONF", "DIV"]]
-    df.to_csv(f"infos_base_{equipe}")
-    
-    
-# Création d'un df pour le stade de l'équipe
-for equipe in liste_equipes:
-    df = get_team_misc(equipe, 2025)
-    df = df[["Arena"]]
-    df.to_csv(f"stade_{equipe}")
-    
-    
+df = get_roster("LAL", 2025)
+# df.drop(columns=["Birth"])
+# df.to_csv(f"roster_LAL") 
+print(df)   
 # TODO: record, next game, last game, coach, salaries, Assistant Coaches and Staff :::::> à coder car pas dans l'API
